ee4699a101c601321c838c6cadc35f12962dcb85
João Jardim <joaopnj19@gmail.com>
2019-06-13 18:09:26 -0300

    Finalizing and test the algoritm

--- services/propagationService.js
+++ services/propagationService.js
@@ -1,28 +1,64 @@
 module.exports = (app) => {
     var neataptic = require('neataptic');
 
-    /* GET home page. */
-
     PropagationService = {
         init : () => {
-            var network = new neataptic.Network(2,1);
 
-            var trainingSet = [
-                { input: [0,0], output: [0] },
-                { input: [0,1], output: [1] },
-                { input: [1,0], output: [1] },
-                { input: [1,1], output: [0] }
-                ];
-                
-            const start = async () => {
-                const result = await network.evolve(trainingSet, {
-                    equal: true,
-                    error: 0.03
+            var dataSets = {
+                'XOR' : [
+                  { input: [0,0], output: [0] },
+                  { input: [0,1], output: [1] },
+                  { input: [1,0], output: [1] },
+                  { input: [1,1], output: [0] }
+                ],
+                'AND' : [
+                  { input: [0,0], output: [0] },
+                  { input: [0,1], output: [0] },
+                  { input: [1,0], output: [0] },
+                  { input: [1,1], output: [1] }
+                ],
+                'OR' : [
+                  { input: [0,0], output: [0] },
+                  { input: [0,1], output: [1] },
+                  { input: [1,0], output: [1] },
+                  { input: [1,1], output: [1] }
+                ]
+            };
+
+            var iterations = 0;
+            var time = 0;
+            var error = 0;
+
+            console.log('');
+
+            console.log('INICIANDO........');
+
+            console.log('');
+
+            console.log('Treinando com os DataSets...');
+
+
+            for(var dataSet in dataSets) {
+                dataSet = dataSets[dataSet];
+
+                var network = new neataptic.architect.Perceptron(2, 4, 1);
+                var results = network.train(dataSet, {
+                    momentum: 0.9,
+                    iterations: 1000,
+                    error: 0.03,
+                    rate: 0.3
                 });
-                console.log(result);
+
+                error += results.error;
+                iterations += results.iterations;
+                time += results.time;
             }
 
-            start();
+            console.log("Iterations: " + iterations);
+            console.log("Time: " + time + 'ms');
+            console.log('---------------------');
+            console.log('');
+
         }
     }
 
